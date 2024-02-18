34bb456a407221631148db6629ec1824b22e85d2
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-02-18 17:23:24 +0000

    use upload method to get user data

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 });
 
-app.post("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "templates/index.html"));
+app.post("/merge", upload.array("pdfs", 2), function (req, res, next) {
+  console.log(req.files);
+  res.send({ data: req.files });
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
 });
 app.listen(3000);
