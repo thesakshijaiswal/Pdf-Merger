7201049bc36540666794382dad664b267787164b
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-02-19 16:48:24 +0000

    create merger pdf function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const multer = require("multer");
+const mergePdfs = require("./merge");
 const upload = multer({ dest: "uploads/" });
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "templates/index.html"));
@@ -9,7 +10,11 @@ app.get("/", function (req, res) {
 
 app.post("/merge", upload.array("pdfs", 2), function (req, res, next) {
   console.log(req.files);
-  res.send({ data: req.files });
+  mergePdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path),
+  );
+  res.redirect("http://localhost:3000");
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
 });
