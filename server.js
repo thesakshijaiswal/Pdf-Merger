a51cc7ddde2520237a067a10ecbd0f3dda2d6e89
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-02-21 17:10:58 +0000

    made regular function async to redirect after merge

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const multer = require("multer");
-const mergePdfs = require("./merge");
+const { mergePdfs } = require("./merge");
 const upload = multer({ dest: "uploads/" });
+app.use("/static", express.static("public"));
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 });
 
-app.post("/merge", upload.array("pdfs", 2), function (req, res, next) {
+app.post("/merge", upload.array("pdfs", 2), async (req, res, next) => {
   console.log(req.files);
-  mergePdfs(
+  await mergePdfs(
     path.join(__dirname, req.files[0].path),
     path.join(__dirname, req.files[1].path),
   );
-  res.redirect("http://localhost:3000");
+  res.redirect("http://localhost:3000/static/merged.pdf");
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
 });
