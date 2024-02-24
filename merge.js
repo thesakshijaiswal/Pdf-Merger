b7b75e0b63ad744b5e9e1ed1fcd5848fb8030f05
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-02-24 13:52:24 +0000

    fix repetitive pdf name

--- merge.js
+++ merge.js
@@ -1,12 +1,11 @@
 import PDFMerger from "pdf-merger-js";
-
 var merger = new PDFMerger();
-
 const mergePdfs = async (p1, p2) => {
   await merger.add(p1); //merge all pages. parameter is the path to file and filename.
   await merger.add(p2); // merge only page 2
-  await merger.save("public/merged.pdf"); //save under given name and reset the internal document
-
+  let d = new Date().getTime();
+  await merger.save("public/${d}.pdf");
+  return d; //save under given name and reset the internal document
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
   // fs.writeSync('merged.pdf', mergedPdfBuffer);
